37b0ae5470442b8b1cede23db53b971e445ee307
aidansunbury <aidansunbury@gmail.com>
2024-11-07 13:35:31 -0800

    added comments

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -11,6 +11,9 @@ export const Dropzone: React.FC<DropzoneProps> = ({ files, setFiles }) => {
     accept: ImageMimeTypes,
     onDrop: (acceptedFiles: File[]) => {
       console.log("Files dropped:", acceptedFiles);
+      //* This is a minor thing, but when doing state updates that rely on the previous state, the previous state should be taken from the callback function
+      //* setFiles((prevFiles) => [...prevFiles, ...acceptedFiles])
+      //* See this part of the react lecture: https://youtu.be/yHFlagmVbB0?si=ssurmeBgDBygy6W4&t=984
       setFiles([...files, ...acceptedFiles]); // Update the state with new files
     },
   });
@@ -19,7 +22,9 @@ export const Dropzone: React.FC<DropzoneProps> = ({ files, setFiles }) => {
     <div className="dropzone">
       <div {...getRootProps()}>
         <input {...getInputProps()} />
-        <div className="text-center">Drop files here or click to select files</div>
+        <div className="text-center">
+          Drop files here or click to select files
+        </div>
       </div>
       <div>
         {files.map((file) => (
